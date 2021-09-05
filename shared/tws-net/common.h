75ff0a0da0e0db499903d7c6f600b72dd2609b71
Amy Zala <fallen.avatar28@gmail.com>
2021-09-05 03:39:59 -0400

    cleanup (changed tws-net to static lib from dll)

--- shared/tws-net/common.h
+++ shared/tws-net/common.h
@@ -3,32 +3,6 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#if !defined(PLATFORM_WINDOWS)
-#define PLATFORM_WINDOWS 0
-#endif
-
-#if !defined(PLATFORM_MAC)
-#define PLATFORM_MAC 0
-#endif
-
-#if !defined(PLATFORM_LINUX)
-#define PLATFORM_LINUX 0
-#endif
-
-#if !PLATFORM_WINDOWS && !PLATFORM_MAC && !PLATFORM_LINUX
-#if _WIN32
-#undef PLATFORM_WINDOWS
-#define PLATFORM_WINDOWS 1
-#include "./win32/common.h"
-#elif __APPLE__
-#undef PLATFORM_MAC
-#define PLATFORM_MAC 1
-#elif __linux__
-#undef PLATFORM_LINUX
-#define PLATFORM_LINUX 1
-#else
-#error Unknown Platform
-#endif
-#endif
+#include "tws-core/common.h"
 
 #endif // __COMMON_H__
